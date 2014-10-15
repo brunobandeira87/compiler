47d4d3c4624c446a28bb5327bf0c641943a29695
Bruno Bandeira <brunobandeira87@gmail.com>
2014-10-15 18:11:40 -0300

    Revert "[primeira versão]"
    This reverts commit 014ace8bd95b7fb6ace914f5574adb408dfd6107.

--- src/parser/GrammarSymbols.java
+++ src/parser/GrammarSymbols.java
@@ -10,6 +10,111 @@ package parser;
  */
 
 
+/*
+
+
+program                  ::=    declarationList;
+
+declarationList          ::=    'LET' definition ('AND' definition)*;
+
+definition               ::=    functionDefinition
+                         |    procedureDefinition
+                         |    variableDefinition;
+
+identifierList           ::=    Identifier (',' Identifier)*;
+
+functionDefinition       ::=    Identifier '(' parametersPrototype ')' '=' 'VALOF' functionBlock;
+
+functionBlock            ::=    '{' commandList resultisCommand '}';
+
+procedureDefinition      ::=    Identifier '(' parametersPrototype ')' 'BE' commandBlock;
+
+varGlobalDefinition      ::=    ('GLOBAL') (intVariableDefinition | boolVariableDefinition);
+
+
+intVariableDefinition    ::=    ('INT' Identifier '=' numberExpression)*;
+
+boolVariableDefinition   ::=    ('BOOL' Identifier '=' booleanExpression)*;
+
+commandBlock             ::=    '{' commandList '}';
+
+parametersPrototype      ::=   (('INT' | 'BOOL') Identifier ((, 'INT' | 'BOOL') Identifier)*)*
+
+
+parametersCallCommand    ::=   (Identifier (, Identifier)*)* 
+
+
+commandList              ::=    command (';' command)*;
+
+command                  ::=    assignmentCommand
+                         |    whileCommand
+                         |    ifCommand
+                         |    breakCommand
+                         |    continueCommand
+                         |    printCommand
+                         |    variableDecCommand
+                         |    callCommand;
+
+callCommand              ::=     functionCallCommand
+                         |      procedureCallCommand
+
+
+variableDecCommand       ::=     intVariableDefinition
+                         |      boolVariableDefinition
+
+functionCallCommand      ::=    Identifier '('(parametersCallCommand)*')'
+
+procedureCallCommand     ::=    Identifier '('(parametersCallCommand)*')'
+
+ifCommand                ::=    'IF' '(' booleanExpression | booleanValue ')' commandBlock ('ELSE' commandBlock)?;
+
+whileCommand             ::=    'WHILE' '(' ( booleanExpression | booleanValue ')' commandBlock;
+
+resultisCommand          ::=    'RESULTIS' expression;
+
+assignmentCommand        ::=    Identifier ':=' (number | booleanValue | numberExpression | functionCallCommand | Identifier);
+
+breakCommand             ::=    'BREAK';
+
+continueCommand          ::=    'CONTINUE';
+
+printCommand             ::=    'WRITEF' '(' (Identifier)* ')';
+
+
+numberBoolExpression     ::=     (number | Identifier) Op_relation ( number | Identifier) 
+
+
+arithBoolExpression      ::= arithmeticExpression | numberBoolExpression
+
+
+booleanExpression        ::= ('!')? (  andExpression | orExpression  | equalExpresion | notEqualExpression )
+
+
+numberExpression         ::=  (multDivExpression)? arithmeticExpression
+arithmeticExpression     ::=  multDivExpression
+                         |   addSubExpression
+
+                         
+multDivExpression        ::=  (Identifier | number) ('/' | '*') (Identifier | number)
+
+addSubExpression         ::=  (Identifier | number) ('+' | '-') (Identifier | number)                         
+
+andExpression            ::=  ( numberBoolExpression |(arithmeticExpression op_rel arithmeticExpression) | booleanValue ) '&&' ( numberBoolExpression |(arithmeticExpression op_rel arithmeticExpression) | booleanValue)
+                         
+orExpression             ::=  ( numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue ) '||' ( numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue )
+
+equalExpression          ::=  (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue  ) '==' (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue )
+
+notEqualExpression       ::=  (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue ) '!=' (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue )
+                         
+
+booleanValue            ::= 'TRUE'
+                        |  'FALSE'
+
+
+
+
+*/
 
 
 
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -6,121 +6,11 @@ import util.AST.AST;
 
 /**
  * Parser class
- * @version 2014-october-15
+ * @version 2010-august-29
  * @discipline Projeto de Compiladores
- * @authors Bruno Bandeira e Bruno Bastos
- * @email (bcabm,babg)@ecomp.poli.br
+ * @author Gustavo H P Carvalho
+ * @email gustavohpcarvalho@ecomp.poli.br
  */
-
-
-
-/*
-
-
-program                  ::=    declarationList;
-
-declarationList          ::=    'LET' definition ('AND' definition)*;
-
-definition               ::=    functionDefinition
-                         |    procedureDefinition
-                         |    variableDefinition;
-
-identifierList           ::=    Identifier (',' Identifier)*;
-
-functionDefinition       ::=    Identifier '(' parametersPrototype ')' '=' 'VALOF' functionBlock;
-
-functionBlock            ::=    '{' commandList resultisCommand '}';
-
-procedureDefinition      ::=    Identifier '(' parametersPrototype ')' 'BE' commandBlock;
-
-varGlobalDefinition      ::=    ('GLOBAL') (intVariableDefinition | boolVariableDefinition);
-
-
-intVariableDefinition    ::=    ('INT' Identifier '=' numberExpression)*;
-
-boolVariableDefinition   ::=    ('BOOL' Identifier '=' booleanExpression)*;
-
-commandBlock             ::=    '{' commandList '}';
-
-parametersPrototype      ::=   (('INT' | 'BOOL') Identifier ((, 'INT' | 'BOOL') Identifier)*)*
-
-
-parametersCallCommand    ::=   (Identifier (, Identifier)*)* 
-
-
-commandList              ::=    command (';' command)*;
-
-command                  ::=    assignmentCommand
-                         |    whileCommand
-                         |    ifCommand
-                         |    breakCommand
-                         |    continueCommand
-                         |    printCommand
-                         |    variableDecCommand
-                         |    callCommand;
-
-callCommand              ::=     functionCallCommand
-                         |      procedureCallCommand
-
-
-variableDecCommand       ::=     intVariableDefinition
-                         |      boolVariableDefinition
-
-functionCallCommand      ::=    Identifier '('(parametersCallCommand)*')'
-
-procedureCallCommand     ::=    Identifier '('(parametersCallCommand)*')'
-
-ifCommand                ::=    'IF' '(' booleanExpression | booleanValue ')' commandBlock ('ELSE' commandBlock)?;
-
-whileCommand             ::=    'WHILE' '(' ( booleanExpression | booleanValue ')' commandBlock;
-
-resultisCommand          ::=    'RESULTIS' expression;
-
-assignmentCommand        ::=    Identifier ':=' (number | booleanValue | numberExpression | functionCallCommand | Identifier);
-
-breakCommand             ::=    'BREAK';
-
-continueCommand          ::=    'CONTINUE';
-
-printCommand             ::=    'WRITEF' '(' (Identifier)* ')';
-
-
-numberBoolExpression     ::=     (number | Identifier) Op_relation ( number | Identifier) 
-
-
-arithBoolExpression      ::= arithmeticExpression | numberBoolExpression
-
-
-booleanExpression        ::= ('!')? (  andExpression | orExpression  | equalExpresion | notEqualExpression )
-
-
-numberExpression         ::=  (multDivExpression)? arithmeticExpression
-arithmeticExpression     ::=  multDivExpression
-                         |   addSubExpression
-
-                         
-multDivExpression        ::=  (Identifier | number) ('/' | '*') (Identifier | number)
-
-addSubExpression         ::=  (Identifier | number) ('+' | '-') (Identifier | number)                         
-
-andExpression            ::=  ( numberBoolExpression |(arithmeticExpression op_rel arithmeticExpression) | booleanValue ) '&&' ( numberBoolExpression |(arithmeticExpression op_rel arithmeticExpression) | booleanValue)
-                         
-orExpression             ::=  ( numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue ) '||' ( numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue )
-
-equalExpression          ::=  (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue  ) '==' (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue )
-
-notEqualExpression       ::=  (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue ) '!=' (numberBoolExpression |(arithmeticExpression op_rel (arithmeticExpression | number)) | booleanValue )
-                         
-
-booleanValue            ::= 'TRUE'
-                        |  'FALSE'
-
-
-
-
-*/
-
-
 public class Parser {
 
 	// The current token
@@ -141,26 +31,17 @@ public class Parser {
 	 * @param kind
 	 * @throws SyntacticException
 	 */ //TODO
-
-        private void accept(int kind) throws SyntacticException {
+	private void accept(int kind) throws SyntacticException {
 		// If the current token kind is equal to the expected
-            if(this.currentToken.equals(kind)){
 			// Gets next token
-                this.currentToken = this.scanner.getNextToken();
-                
-            }
 		// If not
-            else{
 			// Raises an exception
-                throw new SyntacticException(null, currentToken);
-            }
 	}
 	
 	/**
 	 * Gets next token
 	 */ //TODO
 	private void acceptIt() {
-            this.currentToken = this.scanner.getNextToken();
 	}
 
 	/**
@@ -171,177 +52,11 @@ public class Parser {
             this.parseProgram();
 		return null;
 	}
-        
-        
-        private void parseProgram(){
+	private AST parseProgram() throws SyntacticException {
+            
+            return null;
             
         }
-        /*
-        * declarationList          ::=    'LET' definition ('AND' definition)*;
-                                    |      varGlobalDefinition
-        */
-       
-        
-        private void parseDeclarationList() throws SyntacticException{
-            switch(this.currentToken.getKind()){
-                case Token.LET:
-                    acceptIt();
-                    parseDefinition();
-                    while(this.currentToken.getKind() == Token.AND){
-                        acceptIt();
-                        parseDefinition();
-                    }
-                
-                break;
-                
-                case Token.VARGLODEF:
-                    acceptIt();
-                    parseVarGlobalDefinition();
-                break;
-                    
-                default:
-                    throw new SyntacticException(null, currentToken);
-                    
-                    
-            }
-            
-        }
-             
-        /*
-        definition               ::=    functionDefinition
-                         |    procedureDefinition
-                         |    variableDefinition;
-
-        
-        */
-        
-        private void parseDefinition(){
-            switch(this.currentToken.getKind()){
-                
-                case Token.FUNCDEF:
-                    parseFunctionDefiniton();
-                    break;
-                
-                case Token.PROCDEF:
-                    parseProcedureDefinition();
-                    break;
-                    
-                case Token.VARDEF:
-                    parseVarGlobalDefinition();
-                           
-            }
-        }
-        
-        private void parseFunctionDefiniton(){
-        }
-        
-        private void parseProcedureDefinition(){
-        }
-        
-        private void parseVariableDefinitnion(){
-        }
-        
-        private void parseVarGlobalDefinition(){
-        }
-        
-        private void parseIntVariableDefinition(){
-        
-        }
-        
-        private void parseBoolVariableDefinition(){
-        
-        }
-        
-        private void parseFunctionBlock(){
-            
-        }
-        
-        private void parseIdentifierList(){
-        }
-        
-        private void parseParametersPrototype(){
-        }
-        
-        private void parseCommandBlock(){
-        }
-        
-        private void parseCommandList(){
-        }
-        
-        private void parseParametersCallCommand(){
-        }
-        
-        private void parseCommand(){
-        }
-        
-        private void parseAssignmentCommand(){
-        }
-        
-        private void parseWhileCommand(){
-        }
-        
-        private void parseIfCommand(){
-        }
-        
-        private void parseBreakCommand(){
-        }
-        
-        private void parseContinueCommand(){
-        }
-        
-        private void parsePrintCommand(){
-        }
-        
-        private void parseVariableDecCommand(){
-        }
-        
-        private void parseCallCommand(){
-        }
-        
-        private void parseFunctionCallCommand(){
-        }
-        
-        private void parseProcedureCallCommand(){
-        }
-        
-        private void parseResultIsCommand(){
-        }
-        
-        private void parseNumberBoolExpression(){
-            
-        }
-        
-        private void parseArithmeticBoolExpression(){
-        }
-        
-        private void parseBoolExpression(){
-        }
-        
-        private void parseNumberExpression(){
-        }
-        
-        private void parseArithmeticExpression(){
-        }
-        
-        private void parseMultiDivExpression(){
-        }
-        
-        private void parseAddSubExpression(){
-        }
-        
-        private void parseAndExpression(){
-        }
-        
-        private void parseOrExpression(){
-        }
-        
-        private void parseEqualExpression(){
-        }
-        
-        private void parseNotEqualExpression(){
-        }
-        
-        
         
  
 }
